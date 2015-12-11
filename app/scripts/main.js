22579f4ce7e7f00d6f255ab123185e097f09b1a2
Jesse Angelo <jesse@jesseangelo.com>
2015-12-11 16:20:23 -0500

    added fonts, fixed action bar, changed box-shadow behavior when scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -131,7 +131,16 @@ $(".navbar-custom .dropdown").hover(function () {
     $('.menu-backdrop').remove();
   
 });
+
 */
+$(window).scroll(function(){
+  if( $(window).scrollTop() != 0 ) {
+    $('.navbar-custom').addClass('scrolling');
+  } else {
+    $('.navbar-custom').removeClass('scrolling');
+  }
+});
+
 /* end nav scripts */
 
 /* Actionbar scripts */
