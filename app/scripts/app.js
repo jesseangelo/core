10a3ae9ae7c37275fadaeb88afb540303db15f34
jesseangelo <jesse.angelo@ipreo.com>
2016-12-14 15:43:13 -0500

    Added alerts and expanded layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,26 @@
 (function() {
   'use strict';
   
-  var app = angular.module('app', []);
+  var app = angular.module('app', ['ui.bootstrap']);
 
   app.controller('uiController', uiController);
 
   function uiController() {
+      var ctrl = this;
+      ctrl.addAlert = addAlert;
+      ctrl.closeAlert = closeAlert;
+      ctrl.alerts = [
+            //{ type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+            //{ type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+        ];
 
+      function addAlert() {
+          ctrl.alerts.push({msg: 'Yellow alert!'});
+      }
+
+      function closeAlert(index) {
+          ctrl.alerts.splice(index, 1);
+      }
   }
 
 })();
